bee3596088c1f618f6b63d6bba55ad961b6ef2bc
abdel <kikoolaa39@gmail.com>
2020-04-10 10:48:12 +0200

    Petite modif

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      {/* <Contact
+      <Contact
         name = "gean"
         avatar="https://randomuser.me/api/portraits/women/55.jpg"
         online
@@ -13,7 +13,7 @@ function App() {
         name = 'Erica Hunt'
         avatar="https://randomuser.me/api/portraits/women/87.jpg"
         online
-      /> */}
+      />
       <Contact
         name="Nellie Caldwell"
         avatar="https://randomuser.me/api/portraits/women/17.jpg"

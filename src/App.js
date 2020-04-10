1f1bcebb522a257fed86b12d1d271af6ba3ab5fc
abdel <kikoolaa39@gmail.com>
2020-04-10 09:42:21 +0200

    quete react5

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="App">
-      <Contact
-        name = 'Juanita Phillips'
-        online
+      {/* <Contact
+        name = "gean"
         avatar="https://randomuser.me/api/portraits/women/55.jpg"
+        online
       />
       <Contact
         name = 'Erica Hunt'
         avatar="https://randomuser.me/api/portraits/women/87.jpg"
-      />
+        online
+      /> */}
       <Contact
         name="Nellie Caldwell"
-        online
         avatar="https://randomuser.me/api/portraits/women/17.jpg"
       />
     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Style.css";
+import PropTypes from "prop-types";
 
-function Contact(props) {
+
+class Contact extends Component{
+
+
+    state = {
+      online : false,
+    };
+
+  
+
+ handleClick = ()=>{
+  const status = !this.state.online;
+  this.setState({online : status});
+}
+
+
+  render(){
     return (
       <div className = 'main'>
         <div className = 'Contact'>
-          <img classname = 'avatar' src = {props.avatar} alt = {props.name}></img>
+          <img className = 'avatar' src = {this.props.avatar} alt = {this.props.name}/>
           <div className = 'container'>
-            <h4 className = 'name'>{props.name}</h4>
-            <div className = 'status'>
-              {props.online ? 'online' : 'offline'}
-              {props.online ? <span className = 'status-online' /> :  <span className = 'status-offline' />}   
+            <h4 className = 'name'>{this.props.name}</h4>
+              <div className = 'status' onClick = {this.handleClick}>
+                {this.state.online ? 'online' : 'offline'}
+                {this.state.online ? <span className = 'status-online' /> :  <span className = 'status-offline' />}   
+            </div>
           </div>
         </div>
-      </div>
-    </div> 
-  );
+      </div> 
+    );
+  }
 }
 
+Contact.propTypes = {
+  name : PropTypes.string.isRequired,
+  avatar : PropTypes.string.isRequired,
+  online : PropTypes.bool
+};
 
 export default Contact;
 
